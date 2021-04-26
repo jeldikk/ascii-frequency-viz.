7630c0f84aadef3f889e54f4032474e24e2a0e4c
jeldikk <jeldi.kamal2013@gmail.com>
2021-04-26 07:31:16 +0530

    initial point of rendering component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import {Route, Switch} from 'react-router-dom'
+import Helmet from 'react-helmet'
+import './App.scss';
+
+import Header from "./components/header/header.component"
+
+import LettersPage from "./pages/letters/letters.page"
+import WordsPage from "./pages/words/words.page"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
+      </Helmet>
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/letters" component={LettersPage} />
+          <Route path="/words" component={WordsPage} />
+        </Switch>
+      </main>
     </div>
   );
 }
